91563ccbea7ec063a634c244d2b26e1d4a3eab99
Phillip48 <phillippereira02@gmail.com>
2022-04-07 16:45:48 -0400

    added notes

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,16 +1,20 @@
+
 // Global variables
-const apiKey = '753a34ec81da704dc4f3b41a1460fc4f';
+
 // const weatherApiUrl = '';
+const apiKey = '753a34ec81da704dc4f3b41a1460fc4f';
 const searchHistory = [];
 var searchBtn = document.getElementById('search-btn');
-const city = document.getElementById('input-area');
+var inputArea = document.getElementById('input-area');
 
 
+// Days.js time
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
 var weatherApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=753a34ec81da704dc4f3b41a1460fc4f`
 
+// Url reguest for weather data
 function weatherData(weatherApi) {
     fetch(weatherApi)
     .then (function(response) {
@@ -27,7 +31,11 @@ weatherData(weatherApiUrl);
 // Event listner for search button 
 searchBtn.addEventListener('click', function() {
     //Replace ${city} in url with user input
+
     // Create element to add data from weather api to the web page
+
     // append element 
+
+    // Localstorage to save recent user searches // create button for those searches
     
 })
