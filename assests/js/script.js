daec939b4bd044c1847bf33003e8d1fc80a0dee3
Phillip48 <phillippereira02@gmail.com>
2022-04-12 17:49:41 -0400

    Working functions. When button is clicked it takes certain info from data. Didn't get forecast or city name to populate

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,9 @@
 // Global variables
 const apiKey = "753a34ec81da704dc4f3b41a1460fc4f";
-const searchHistory = [];
+var searchHistory = [];
 var searchBtn = document.getElementById("search-btn");
 var inputArea = document.getElementById("input-area");
-var weatherApiUrl = `https://api.openweathermap.org`;
-
-var searchHistoryContainer = document.getElementById(
-  "search-history-container"
-);
+var searchHistoryContainer = document.getElementById("search-history-container");
 var todayWeather = document.getElementById("today");
 var forecastWeather = document.getElementById("forecast");
 
@@ -17,101 +13,100 @@ var forecastWeather = document.getElementById("forecast");
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-function displayWeatherData() {
-  //var temp = ;
-  //var windSpeed = ;
-  //var humidity = ;
-  //var uvIndex = ;
-  // display current weather
-}
-
 function forecast() {
   // 5 day forecast
   cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format("M/D/YYYY");
 }
 
 function renderItems(city, data) {
-  displayWeatherData(city, data.current, data.timezone);
+  //displayWeatherData(city, data.current, data.timezone);
   forecast(data.daily, data.timezone);
 }
 
 // Url request for weather data
-function weatherData() {
-  var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputArea.value}&limit=1&appid=${apiKey}`;
-
-  //Error in code...
-  //var coorUrl = `${weatherApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
-
+function weatherData(city) {
+  var cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  inputArea.textContent = document.getElementById("city-input")
   fetch(cityUrl)
     .then(function (response) {
       return response.json();
     })
-    .then(function (cityResponse) {
-      //lat = cityResponse[0].lat;
-      //lon = cityResponse[0].lon;
-      //renderItems(city, cityResponse);
-      return cityResponse;
-    })
     .then(function (regularResponse) {
-        var lat = regularResponse[0].lat;
-        var lon = regularResponse[0].lon;
-        var coorUrl = `${weatherApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
-        fetch(coorUrl)
-          .then(function (response) {
-            return response.json();
-          })
-          .then(function (coorResponse) {
-            console.log(coorResponse);
-          });
+      var lat = regularResponse[0].lat;
+      var lon = regularResponse[0].lon;
+      console.log(lat, lon);
+      displayWeatherData(lat, lon);
+    });
+}
+
+function displayWeatherData(lat, lon) {
+  var coorUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`;
+  console.log(coorUrl);
+  fetch(coorUrl)
+    .then(function (weatherData) {
+      return weatherData.json();
+    })
+    .then(function (weatherResults) {
+      var temp = document.getElementById('temp');
+      var windSpeed = document.getElementById('windSpeed');
+      var humidity = document.getElementById('humidity');
+      var uvIndex = document.getElementById('uvIndex');
+
+      temp.textContent = weatherResults.current.temp;
+      windSpeed.textContent = weatherResults.current.wind_speed;
+      humidity.textContent = weatherResults.current.humidity;
+      uvIndex.textContent = weatherResults.current.uvi;
+
+      var dailyTemp = document.getElementById('dailytemp');
+      var dailyWindSpeed = document.getElementById('dailywindSpeed');
+      var dailyHumidity = document.getElementById('dailyhumidity');
+
+      dailyTemp.textContent = weatherResults.daily[0].temp;
+      dailyWindSpeed.textContent = weatherResults.daily.wind_speed;
+      dailyHumidity.textContent = weatherResults.daily.humidity;
+      // display current weather
     });
 }
 
 // Event listner for search button
-function userResponse() {
+function userResponse(event) {
+  event.preventDefault();
   var userInput = inputArea;
   var userValue = inputArea.value.trim();
-  //Replace ${city} in url with user input
-  //inputArea.replace(weatherApiUrl.cityName)
+  //var addHistory = userValue.push(searchHistory)
+
   // if statement to make sure user inputs text
   if (userValue === "") {
     window.alert("Please enter a value");
   } else {
-    // get local storage
-    JSON.parse(window.localStorage.getItem(userInput));
+    weatherData(userValue);
+    userValue.textContent = document.getElementById('city-input')
   }
-  // Localstorage to save recent user searches // create button for those searches
-  window.localStorage.setItem("city", userInput);
-
-  weatherData();
+  displayHistory();
 }
 
-function displayHistory() {
+function displayHistory(search) {
+  inputArea.innerHTML = "";
   // empty string to push into //searchHistory-already created
+  var addHistory = localStorage.getItem("searchHistory");
+
+  if (addHistory != undefined) {
+    searchHistory = JSON.parse(addHistory);
+  }
+  searchHistory.push(search);
 
   // for loop go through history array, setattr
-  for (let i = 0; i < searchHistory.length; i++) {
-    var forHistory = searchHistory[index];
-    // change variable name
+  for (let i = searchHistory.length - 1; i >= 0; i--) {
+    var forHistory = searchHistory[i];
+
     var previousSearch = document.createElement("button");
     previousSearch.textContent = forHistory;
 
-    searchHistoryContainer.append(previousSearch);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    //searchHistoryContainer.append(forHistory);
   }
+  //console.log(forHistory);
+  //console.log(searchHistory);
 }
-
-function updateLocal(search) {
-  // update localstorage,
-  //JSON strinify to turn search history into json format
-}
-
-function pullHistory() {
-  //pull history from local storage
-  //JSON.parse(window.localStorage.getItem(userInput));
-  //json parse whatever is stringified
-  //window.localStorage.setItem('city', userInput);
-}
-
-
-
 
 searchBtn.addEventListener("click", userResponse);
