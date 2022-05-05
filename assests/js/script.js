813a8a9183f71b286b5a4758974405f6825004bf
Phillip48 <phillippereira02@gmail.com>
2022-05-05 17:37:07 -0400

    Made new forcast cards and data now appends to the cards

--- assests/js/script.js
+++ assests/js/script.js
@@ -15,7 +15,14 @@ dayjs.extend(window.dayjs_plugin_timezone);
 
 function forecast() {
   // 5 day forecast
+  // const today = new Date()
+  // const tomorrow = new Date(today);
+  // let dateCard = tomorrow.setDate(tomorrow.getDate() + 1)
+  // let cardTitle = document.getElementById('future-date')
+  // cardTitle.textContent = datecard;
   cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format("M/D/YYYY");
+
+
 }
 
 function renderItems(city, data) {
@@ -61,14 +68,51 @@ function displayWeatherData(lat, lon) {
       windSpeed.textContent = weatherResults.current.wind_speed;
       humidity.textContent = weatherResults.current.humidity;
       uvIndex.textContent = weatherResults.current.uvi;
-      
-      var dailyTemp = document.getElementById('dailytemp');
-      var dailyWindSpeed = document.getElementById('dailywindSpeed');
-      var dailyHumidity = document.getElementById('dailyhumidity');
 
-      dailyTemp.textContent = weatherResults.daily[0].temp;
-      dailyWindSpeed.textContent = weatherResults.daily.wind_speed;
-      dailyHumidity.textContent = weatherResults.daily.humidity;
+      // card 1
+      var dailyTemp1 = document.getElementById('dailytemp');
+      var dailyWindSpeed1 = document.getElementById('dailywindSpeed');
+      var dailyHumidity1 = document.getElementById('dailyhumidity');
+
+      dailyTemp1.textContent = weatherResults.daily[0].temp.day;
+      dailyWindSpeed1.textContent = weatherResults.daily[0].wind_speed;
+      dailyHumidity1.textContent = weatherResults.daily[0].humidity;
+
+      // card 2
+      var dailyTemp2 = document.getElementById('dailytemp2');
+      var dailyWindSpeed2 = document.getElementById('dailywindSpeed2');
+      var dailyHumidity2 = document.getElementById('dailyhumidity2');
+
+      dailyTemp2.textContent = weatherResults.daily[1].temp.day;
+      dailyWindSpeed2.textContent = weatherResults.daily[1].wind_speed;
+      dailyHumidity2.textContent = weatherResults.daily[1].humidity;
+
+      // card 3
+      var dailyTemp3 = document.getElementById('dailytemp3');
+      var dailyWindSpeed3 = document.getElementById('dailywindSpeed3');
+      var dailyHumidity3 = document.getElementById('dailyhumidity3');
+
+      dailyTemp3.textContent = weatherResults.daily[2].temp.day;
+      dailyWindSpeed3.textContent = weatherResults.daily[2].wind_speed;
+      dailyHumidity3.textContent = weatherResults.daily[2].humidity;
+
+      // card 4
+      var dailyTemp4 = document.getElementById('dailytemp4');
+      var dailyWindSpeed4 = document.getElementById('dailywindSpeed4');
+      var dailyHumidity4 = document.getElementById('dailyhumidity4');
+
+      dailyTemp4.textContent = weatherResults.daily[3].temp.day;
+      dailyWindSpeed4.textContent = weatherResults.daily[3].wind_speed;
+      dailyHumidity4.textContent = weatherResults.daily[3].humidity;
+
+      // card 5
+      var dailyTemp5 = document.getElementById('dailytemp5');
+      var dailyWindSpeed5 = document.getElementById('dailywindSpeed5');
+      var dailyHumidity5 = document.getElementById('dailyhumidity5');
+
+      dailyTemp5.textContent = weatherResults.daily[4].temp.day;
+      dailyWindSpeed5.textContent = weatherResults.daily[4].wind_speed;
+      dailyHumidity5.textContent = weatherResults.daily[4].humidity;
       // display current weather
     });
 }
@@ -76,18 +120,21 @@ function displayWeatherData(lat, lon) {
 // Event listner for search button
 function userResponse(event) {
   event.preventDefault();
-  var userInput = inputArea;
+  // var userInput = inputArea;
   var userValue = inputArea.value.trim();
   //var addHistory = userValue.push(searchHistory)
+  console.log(userValue);
 
   // if statement to make sure user inputs text
   if (userValue === "") {
     window.alert("Please enter a value");
   } else {
     weatherData(userValue);
-    userValue.textContent = document.getElementById('city-input')
+    forecast();
+    // userValue.textContent = document.getElementById('city-input')
   }
   displayHistory();
+
 }
 
 function displayHistory(search) {
